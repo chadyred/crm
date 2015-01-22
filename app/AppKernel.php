42e8878882b72efaf4b1c5104e4abff38baf1d99
rp <remi.poignon@enigmatic.fr>
2015-01-22 14:15:51 +0100

    update : activation de fos_user_bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Ornicar\ApcBundle\OrnicarApcBundle(),
@@ -29,6 +30,7 @@ class AppKernel extends Kernel
             new Avalanche\Bundle\ImagineBundle\AvalancheImagineBundle(),
             new Liip\ThemeBundle\LiipThemeBundle(),
             new BCC\CronManagerBundle\BCCCronManagerBundle(),
+            new UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
