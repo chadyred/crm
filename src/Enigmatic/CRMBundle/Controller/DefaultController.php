04371a7de30cadaffc43be3684277cd79aa08c92
rp <remi.poignon@enigmatic.fr>
2015-02-05 16:13:41 +0100

    update : generale

--- src/Enigmatic/CRMBundle/Controller/DefaultController.php
+++ src/Enigmatic/CRMBundle/Controller/DefaultController.php
@@ -2,12 +2,19 @@
 
 namespace Enigmatic\CRMBundle\Controller;
 
+use Enigmatic\CRMBundle\Entity\Agency;
+use Enigmatic\CRMBundle\Form\AgencyType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('EnigmaticCRMBundle:Default:index.html.twig');
+
+
+        return $this->render('EnigmaticCRMBundle:Default:index.html.twig', array(
+
+        ));
     }
 }
